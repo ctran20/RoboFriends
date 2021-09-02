32fde3f0255b02a7a1e6fa48a1c86db905e1fd4a
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-09-02 15:46:17 -0400

    Added service worker for PWA

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import { Provider } from 'react-redux';
 import { searchRobots, requestRobots, addChange } from './redux/reducer';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
+import * as serviceWorker from './serviceWorker';
+import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'tachyons';
 
@@ -25,4 +27,5 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+serviceWorker.register();
 reportWebVitals();
