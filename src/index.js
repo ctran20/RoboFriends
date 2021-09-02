b959d304002c7c52ff8dbbf21600be45e5f70f3d
Cat Tran <54409102+ctran20@users.noreply.github.com>
2021-09-02 15:48:45 -0400

    Fix small bug

--- src/index.js
+++ src/index.js
@@ -6,7 +6,6 @@ import { searchRobots, requestRobots, addChange } from './redux/reducer';
 import App from './containers/App';
 import reportWebVitals from './reportWebVitals';
 import * as serviceWorker from './serviceWorker';
-import reportWebVitals from './reportWebVitals';
 import './index.css';
 import 'tachyons';
 
@@ -24,8 +23,9 @@ ReactDOM.render(
   document.getElementById('root')
 );
 
+serviceWorker.register();
+
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-serviceWorker.register();
 reportWebVitals();
